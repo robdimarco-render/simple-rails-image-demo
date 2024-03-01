a5c999613fffd456a1b84b254e970ee1a05b76ce
Rob Di Marco <robdimarco@render.com>
2024-03-01 18:37:02 -0500

    Fix skip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
-  skip_before_filter :verify_authenticity_token  
+  skip_before_action :verify_authenticity_token  
 
 
   # GET /users or /users.json
